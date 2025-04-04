62499e32a434b9003d3695681a44e236fa4ae4d1
uzay <uzayilica2@gmail.com>
2025-04-04 20:46:04 +0300

    controllera eklemelerp yapıldı

--- src/main/java/com/example/tsh/controller/UserController.java
+++ src/main/java/com/example/tsh/controller/UserController.java
@@ -1,7 +1,13 @@
 package com.example.tsh.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    @GetMapping("/DENEME")
+    public ResponseEntity<?>getDeneme(){
+        return ResponseEntity.ok().body("deneme");
+    }
 }
---
+++ src/main/java/com/example/tsh/service/UserService.java
@@ -0,0 +1,7 @@
+package com.example.tsh.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
